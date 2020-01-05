9aa35981583d649437e63c5a06c4ea93517eb472
imataka7 <imataka7@gmail.com>
2020-01-05 19:18:35 +0900

    Some dubg

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,6 +5,13 @@
     <p>You received at {{ currentTime }}</p>
     <p>Diff: {{ currentTime - firestoreTime }} msec</p>
     <button @click="push">push</button>
+
+    <input type="text" v-model="text" />
+    <button @click="search">search</button>
+    {{ id }}
+    <p></p>
+    <div class="video-player"></div>
+    <pre>{{ list }}</pre>
   </div>
 </template>
 
@@ -13,6 +20,10 @@
 import {
   Component, Vue, Prop, Watch,
 } from 'vue-property-decorator';
+import YouTube from 'youtube-player';
+import { YouTubePlayer } from 'youtube-player/dist/types';
+import sleep from '../utils/sleep';
+import searchVideo from '@/utils/search';
 
 @Component
 export default class About extends Vue {
@@ -50,6 +61,53 @@ export default class About extends Vue {
       test: Date.now(),
       pushedBy: this.$auth.currentUser?.displayName,
     });
+  }
+
+  public text = '白日';
+
+  public player!: YouTubePlayer;
+
+  public mounted() {
+    const el = this.$el.querySelector('.video-player');
+    this.player = YouTube(el as HTMLElement);
+  }
+
+  public list: readonly string[] | undefined | null= [];
+
+  public async waitUntilPlaylistLoaded() {
+    this.list = null;
+
+    await new Promise((resolve) => {
+      const listener = this.player.on('stateChange', async (event) => {
+        this.list = await this.player.getPlaylist();
+
+        if (this.list !== null) {
+          // Somehow `off` doesn't exist in the type definition.
+          // @ts-ignore
+          this.player.off(listener);
+          resolve();
+        }
+      });
+    });
+    // this.player.removeEventListener('stateChange', )
+  }
+
+  public id = '';
+
+  public async search() {
+    const start = performance.now();
+
+    const id = await searchVideo(this.text);
+    if (!id) {
+      console.log('not found');
+      return;
+    }
+
+    this.player.loadVideoById(id);
+
+    this.id = id;
+
+    console.log(performance.now() - start);
   }
 }
 </script>
