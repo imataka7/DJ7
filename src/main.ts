d3545ae3ab0fce31895e610eb5b284fdfc18c6fb
imataka7 <imataka7@gmail.com>
2020-01-11 02:20:00 +0900

    Add fontawesome to dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import Vue from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faPlay, faPause, faForward, faVolumeMute, faVolumeUp, faVolumeDown, faVolumeOff,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
 
 import firebase from './plugins/firebase';
+
+library.add(faPlay, faPause, faForward, faVolumeMute, faVolumeUp, faVolumeDown, faVolumeOff);
+Vue.component('fa-icon', FontAwesomeIcon);
 
 Vue.use(firebase);
 Vue.config.productionTip = false;
