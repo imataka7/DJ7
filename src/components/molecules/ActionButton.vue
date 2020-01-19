18db7fefefe4264f181b6a3a584274320e079a3f
imataka7 <imataka7@gmail.com>
2020-01-20 00:32:15 +0900

    Improve button style

--- src/components/molecules/ActionButton.vue
+++ src/components/molecules/ActionButton.vue
@@ -1,5 +1,9 @@
 <template>
-  <button class="button action-button" :title="title" @click="$emit('click')">
+  <button
+    :class="`action-button ${isPhone ? '' : 'has-effect'}`"
+    :title="title"
+    @click="$emit('click')"
+  >
     <slot></slot>
   </button>
 </template>
@@ -9,11 +13,16 @@
 import {
   Component, Vue, Prop, Watch,
 } from 'vue-property-decorator';
+import isMobile from 'ismobilejs';
 
 @Component
 export default class ActionButton extends Vue {
   @Prop({ default: '' })
   public title!: string;
+
+  get isPhone() {
+    return isMobile().phone;
+  }
 }
 </script>
 
@@ -24,8 +33,53 @@ export default class ActionButton extends Vue {
   border-radius: 9999px;
   border: 1px solid #ccc;
   background: #fff;
-  cursor: pointer;
   font-size: 0.9em;
+}
+
+.action-button.has-effect {
+  --button-color: rgba(255, 85, 0, 1);
+  --border-color: #ccc;
+  --border-color-hover: #777;
+
+  position: relative;
+  cursor: pointer;
+
+  &:not(:disabled) {
+    &:hover {
+      border-color: var(--button-color);
+      color: var(--button-color);
+    }
+
+    &:after {
+      content: "";
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      border-radius: 9999px;
+      opacity: 0;
+      background: #f50;
+      transition: all 0.8s;
+    }
+
+    &:active:after {
+      // background: #f50;
+      opacity: 0.8;
+      transition: all 0s;
+    }
+
+    &:focus {
+      outline: 0;
+      box-shadow: 0px 0px 3px var(--button-color);
+      border-color: var(--button-color);
+    }
+  }
+
+  &:disabled {
+    color: #ddd;
+    cursor: not-allowed;
+  }
 }
 
 @media screen and (max-width: 480px) {
@@ -33,83 +87,6 @@ export default class ActionButton extends Vue {
     width: 50px;
   }
 }
-
-// .action-button {
-//   --button-color: rgba(255, 85, 0, 1);
-//   --button-width: 70px;
-//   --button-height: 25px;
-//   --border-color: #ccc;
-//   --border-color-hover: #777;
-
-//   cursor: pointer;
-//   position: relative;
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   min-width: var(--button-width);
-//   height: var(--button-height);
-//   border: solid 1px;
-//   border-radius: 9999px;
-//   font-size: 0.9em;
-//   border-color: var(--border-color);
-//   transition: all 0.1s;
-//   padding: 0;
-//   color: #333;
-//   background: #fff;
-
-//   &:not(:disabled) {
-//     &:hover {
-//       border-color: var(--button-color);
-//       color: var(--button-color);
-//     }
-
-//     &:after {
-//       content: "";
-//       position: absolute;
-//       width: 100%;
-//       height: 100%;
-//       border-radius: 9999px;
-//       opacity: 0;
-//       background: #f50;
-//       transition: all 0.8s;
-//     }
-
-//     &:active:after {
-//       // background: #f50;
-//       opacity: 0.8;
-//       transition: 0s;
-//     }
-
-//     &:focus {
-//       outline: 0;
-//       box-shadow: 0px 0px 3px var(--button-color);
-//       border-color: var(--button-color);
-//     }
-//   }
-
-//   &:disabled {
-//     color: #ddd;
-//     cursor: not-allowed;
-//   }
-// }
-
-// @media screen and (max-width: 480px) {
-//   .action-button {
-//     min-width: 0;
-//     width: 50px;
-
-//     &:not(:disabled):hover {
-//       &:hover {
-//         color: initial;
-//         border-color: var(--border-color);
-//       }
-
-//       &:after {
-//         transition: all 0.5s;
-//       }
-//     }
-//   }
-// }
 
 @keyframes button-flush {
   0%,
