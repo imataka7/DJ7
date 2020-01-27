d7cad6296e90940f1c8a6c33b525efcb5457d840
imataka7 <imataka7@gmail.com>
2020-01-27 22:30:02 +0900

    Add firebase analytics

--- src/plugins/firebase.ts
+++ src/plugins/firebase.ts
@@ -4,6 +4,7 @@ import Vue, { VueConstructor, PluginObject } from 'vue';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/analytics';
 
 declare module 'vue/types/vue' {
   interface Vue {
@@ -25,10 +26,11 @@ const firebaseConfig = {
   storageBucket: 'sairilab-musichub.appspot.com',
   messagingSenderId: '1040913343225',
   appId: '1:1040913343225:web:55642f366ef729023a7828',
-  measurementId: 'G-78Z8YSTCVZ',
+  measurementId: 'G-80Z8YSTCVZ',
 };
 
 export const app = firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 export default {
   install(vue: VueConstructor, options: any) {
