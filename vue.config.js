bccdffae7697608eda2219cb1f3175675dd8b779
imataka7 <imataka7@gmail.com>
2020-02-02 22:23:05 +0900

    Chenge PWA settings

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,15 @@
 process.env.VUE_APP_VERSION = 'v0.21.6 on 20200201';
+
+const manifestJSON = require('./public/manifest.json');
+
+pwaArgs = {
+  themeColor: manifestJSON.theme_color,
+  name: manifestJSON.short_name,
+  msTileColor: manifestJSON.background_color,
+};
+
+module.exports = {
+  chainWebpack: (config) => {
+    config.plugin('pwa').tap(() => [pwaArgs]);
+  },
+};
