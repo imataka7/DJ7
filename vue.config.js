387d4dcabcbffa9e95f1af2e2bd8fd8874e36809
imataka7 <imataka7@gmail.com>
2020-02-05 22:09:19 +0900

    Configure workbox

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-process.env.VUE_APP_VERSION = 'v0.23.0 on 20200204';
+process.env.VUE_APP_VERSION = 'v0.23.4 on 20200205';
 
 const manifestJSON = require('./public/manifest.json');
 
@@ -8,7 +8,46 @@ const pwaArgs = {
   msTileColor: manifestJSON.background_color,
 };
 
+const dist = `${__dirname}/dist`;
+
 module.exports = {
+  pwa: {
+    workboxPluginMode: 'GenerateSW',
+    workboxOptions: {
+      swDest: `${dist}/service-worker.js`,
+      skipWaiting: true,
+      clientsClaim: true,
+      runtimeCaching: [
+        // {
+        //   handler: 'NetworkFirst',
+        //   urlPattern: /\/index\.html/,
+        //   options: {
+        //     cacheName: 'dj7-index',
+        //     expiration: {
+        //       maxAgeSeconds: 60 * 60 * 24,
+        //     },
+        //   },
+        // },
+        {
+          handler: 'StaleWhileRevalidate',
+          urlPattern: /^https:\/\/fonts\.google\.com/,
+          options: {
+            cacheName: 'google-fonts-stylesheets',
+          },
+        },
+        {
+          handler: 'CacheFirst',
+          urlPattern: /^https:\/\/fonts\.gstatic\.com/,
+          options: {
+            cacheName: 'google-fonts-webfonts',
+            expiration: {
+              maxAgeSeconds: 60 * 60 * 24 * 365,
+            },
+          },
+        },
+      ],
+    },
+  },
   chainWebpack: (config) => {
     config.plugin('pwa').tap(() => [pwaArgs]);
   },
