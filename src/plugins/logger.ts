27aa6d02acbd0be657a2f7706a4fefbdf5e0bcd0
imataka7 <imataka7@gmail.com>
2020-05-03 23:18:07 +0900

    Disable sentry

--- src/plugins/logger.ts
+++ src/plugins/logger.ts
@@ -2,10 +2,10 @@ import Vue, { VueConstructor, PluginObject } from 'vue';
 import * as Sentry from '@sentry/browser';
 import { Vue as IntegrationsVue } from '@sentry/integrations';
 
-Sentry.init({
-  dsn: 'https://9d2272a9d5c14d2c8697df2b5e827c37@sentry.io/2538898',
-  integrations: [new IntegrationsVue({ Vue, attachProps: true, logErrors: true })],
-});
+// Sentry.init({
+//   dsn: 'https://9d2272a9d5c14d2c8697df2b5e827c37@sentry.io/2538898',
+//   integrations: [new IntegrationsVue({ Vue, attachProps: true, logErrors: true })],
+// });
 
 type UserInfo = {
   uid: string;
@@ -19,30 +19,29 @@ type Context = {
   action: string;
   roomId: string;
   content: any;
-  // width: number;
 }
 
 function initUserInfo() {
-  Sentry.setUser({
-    username: 'Anonymous',
-    version: process.env.VUE_APP_VERSION,
-  });
+  // Sentry.setUser({
+  //   username: 'Anonymous',
+  //   version: process.env.VUE_APP_VERSION,
+  // });
 }
 
 function setUserInfo(info: UserInfo) {
-  Sentry.setUser({
-    ...info,
-    version: process.env.VUE_APP_VERSION,
-  });
+  // Sentry.setUser({
+  //   ...info,
+  //   version: process.env.VUE_APP_VERSION,
+  // });
 }
 
 function captureException(error: any) {
-  Sentry.captureException(error);
+  // Sentry.captureException(error);
 }
 
 const loggerBase = (level: Sentry.Severity, message: string, context: Partial<Context> = {}) => {
-  Sentry.setExtras({ ...context, width: window.innerWidth });
-  Sentry.captureMessage(message, level);
+  // Sentry.setExtras({ ...context, width: window.innerWidth });
+  // Sentry.captureMessage(message, level);
 };
 
 const info = (m: string, c: Partial<Context> = {}) => loggerBase(Sentry.Severity.Info, m, c);
