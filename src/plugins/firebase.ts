9da170a1ebc61350cba4ce1ea88965ce360d2682
imataka7 <imataka7@gmail.com>
2020-05-04 11:19:29 +0900

    Make possible to switching environment

--- src/plugins/firebase.ts
+++ src/plugins/firebase.ts
@@ -20,7 +20,7 @@ declare module 'vue/types/vue' {
   }
 }
 
-const firebaseConfig = {
+const firebaseConfigProd = {
   apiKey: 'AIzaSyDNymPlrtN1h5LM7DSg7YLyEw2CEQa8gNY',
   authDomain: 'sairilab-musichub.firebaseapp.com',
   databaseURL: 'https://sairilab-musichub.firebaseio.com',
@@ -30,6 +30,19 @@ const firebaseConfig = {
   appId: '1:1040913343225:web:55642f366ef729023a7828',
   measurementId: 'G-80Z8YSTCVZ',
 };
+
+const firebaseConfigDev = {
+  apiKey: 'AIzaSyD3eOIBzcsGOfLpHeGYnVy03oKTqzDIj-s',
+  authDomain: 'dj7-debug.firebaseapp.com',
+  databaseURL: 'https://dj7-debug.firebaseio.com',
+  projectId: 'dj7-debug',
+  storageBucket: 'dj7-debug.appspot.com',
+  messagingSenderId: '455043296430',
+  appId: '1:455043296430:web:d0051e97cd1bf8730e113f',
+  measurementId: 'G-QGQNJCWVCP',
+};
+
+const firebaseConfig = process.env.NODE_ENV === 'production' ? firebaseConfigProd : firebaseConfigDev;
 
 export const app = firebase.initializeApp(firebaseConfig);
 const anal = firebase.analytics();
