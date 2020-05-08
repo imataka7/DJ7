b7d706f724a23c1b6c7340ef89dfb6b04049d0b0
imataka7 <imataka7@gmail.com>
2020-05-08 11:31:01 +0900

    Add rule to eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'quotes': ['error', 'single'],
+    'object-curly-spacing': ['error', 'always'],
     'no-async-promise-executor': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     '@typescript-eslint/ban-ts-ignore': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     '@typescript-eslint/no-var-requires': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
