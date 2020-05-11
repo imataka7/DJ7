1bd2dbad4d87021a5fd1f5c6cd4be2e4536ec413
fuyutarow <fuyutarow@gmail.com>
2020-05-11 21:14:55 +0900

    Fix typo

--- src/views/Hub/script.ts
+++ src/views/Hub/script.ts
@@ -47,7 +47,7 @@ export default class Hub extends Vue {
   // RoleTagから論理話をとってDJ操作の可不可を算出
   // (Government, Array<RoleTag>) -> Boolean
   get role(): Role {
-    // anarchims
+    // anarchism
     if (!room.isMonarchism) {
       return roleBook['managePlay']
     } else if (!this.currentUser) {
