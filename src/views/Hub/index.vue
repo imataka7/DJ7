4687363f61253717f4db9bbe48772ba6539ee405
imataka7 <imataka7@gmail.com>
2020-06-01 22:56:37 +0900

    Destory musichub

--- src/views/Hub/index.vue
+++ src/views/Hub/index.vue
@@ -6,10 +6,15 @@
           <div class="room-desc">
             <div class="dj7-logo">
               <img :src="require('@/assets/logo.png')" alt="DJ7" />
-              <fa-icon class="crown" icon="crown" size="lg" v-if="room.isMonarchism"></fa-icon>
+              <fa-icon
+                class="crown"
+                icon="crown"
+                size="lg"
+                v-if="room.isMonarchism"
+              ></fa-icon>
             </div>
 
-            <div v-if="dbg" style="border: solid 1px red;">
+            <div v-if="false" style="border: solid 1px red;">
               <div>government: {{ room.government }}</div>
               <div>adminUsers: {{ room.adminUsers }}</div>
               <div>role: {{ role }}</div>
@@ -20,35 +25,36 @@
               <div>
                 <button
                   @click="$router.push({ name: 'hub', params: { roomId } })"
-                >RoomId: {{ roomId }}</button>
+                >
+                  RoomId: {{ roomId }}
+                </button>
               </div>
               <div v-if="role.manageUser">
                 <button
                   @click="$router.push({ name: 'admin', params: { roomId } })"
-                >{{ `${roomId}/admin` }}</button>
+                >
+                  {{ `${roomId}/admin` }}
+                </button>
               </div>
             </div>
 
             <p>
               部屋ID: {{ roomId }}
-              <!-- <template v-if="role.manageUser"> -->
-              <abutton @click="$router.push({ name: 'admin', params: { roomId } })">
+              <abutton
+                @click="$router.push({ name: 'admin', params: { roomId } })"
+              >
                 設定
                 <fa-icon icon="cog"></fa-icon>
               </abutton>
-              <!-- </template>
-              <template v-else>
-                <abutton @click="$router.push({ name: 'admin', params: { roomId } })">
-                  設定
-                  <fa-icon icon="cog"></fa-icon>
-                </abutton>
-              </template>-->
             </p>
             <span class="version">{{ version }}</span>
           </div>
 
           <div class="share-button-container">
-            <share-button :room-id="roomId" :now-playing="playingMusic && playingMusic.title"></share-button>
+            <share-button
+              :room-id="roomId"
+              :now-playing="playingMusic && playingMusic.title"
+            ></share-button>
           </div>
 
           <div class="room-users">
@@ -67,7 +73,12 @@
             <section>
               <label>
                 <p class="label-desc">部屋の移動/作成をする</p>
-                <input type="text" v-model="jumpTo" :disabled="!currentUser" placeholder="部屋ID" />
+                <input
+                  type="text"
+                  v-model="jumpTo"
+                  :disabled="!currentUser"
+                  placeholder="部屋ID"
+                />
                 <abutton @click="jump" :disabled="!currentUser">移動</abutton>
               </label>
             </section>
@@ -78,12 +89,16 @@
               </label>
             </section>
           </div>
-          <abutton @click="signOut" :disabled="!currentUser">サインアウト</abutton>
+          <abutton @click="signOut" :disabled="!currentUser"
+            >サインアウト</abutton
+          >
         </div>
 
         <div class="column swiper-slide">
           <p class="header">キュー</p>
-          <div class="no-music" v-if="queues.length === 0">キューに動画がありません</div>
+          <div class="no-music" v-if="queues.length === 0">
+            キューに動画がありません
+          </div>
           <music-queue
             v-model="queues"
             @interrupt="interrupt"
@@ -99,10 +114,14 @@
           <template v-if="!currentUser">
             <div class="no-music">Only available for signed in users</div>
             <div class="button-container">
-              <abutton class="is-large" @click="$router.push('/signin')">サインイン</abutton>
+              <abutton class="is-large" @click="$router.push('/signin')"
+                >サインイン</abutton
+              >
             </div>
           </template>
-          <div class="no-music" v-else-if="history.length === 0">履歴に動画がありません</div>
+          <div class="no-music" v-else-if="history.length === 0">
+            履歴に動画がありません
+          </div>
           <history-list
             :list="history"
             @add="addQueue"
