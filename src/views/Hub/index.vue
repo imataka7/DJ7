f59062de3755946a69625db95e0cd66fa25fbe3d
imataka7 <imataka7@gmail.com>
2020-06-03 22:13:52 +0900

    ─調整─

--- src/views/Hub/index.vue
+++ src/views/Hub/index.vue
@@ -1,136 +1,133 @@
 <template>
   <div class="hub">
-    <div class="swiper-container">
-      <div class="columns swiper-wrapper">
-        <div class="column swiper-slide input-container">
-          <div class="room-desc">
-            <div class="dj7-logo">
-              <img :src="require('@/assets/logo.png')" alt="DJ7" />
-              <fa-icon
-                class="crown"
-                icon="crown"
-                size="lg"
-                v-if="room.isMonarchism"
-              ></fa-icon>
-            </div>
+    <div class="columns swiper-wrapper">
+      <div class="column swiper-slide input-container">
+        <div class="room-desc">
+          <div class="dj7-logo">
+            <img :src="require('@/assets/logo.png')" alt="DJ7" />
+            <fa-icon
+              class="crown"
+              icon="crown"
+              size="lg"
+              v-if="room.isMonarchism"
+            ></fa-icon>
+          </div>
 
-            <div v-if="false" style="border: solid 1px red;">
-              <div>government: {{ room.government }}</div>
-              <div>adminUsers: {{ room.adminUsers }}</div>
-              <div>role: {{ role }}</div>
-              <div>users: {{ room && room.users }}</div>
-              <div v-if="currentUser">
-                <div>uid: {{ currentUser.uid }}</div>
-              </div>
-              <div>
-                <button
-                  @click="$router.push({ name: 'hub', params: { roomId } })"
-                >
-                  RoomId: {{ roomId }}
-                </button>
-              </div>
-              <div v-if="role.manageUser">
-                <button
-                  @click="$router.push({ name: 'admin', params: { roomId } })"
-                >
-                  {{ `${roomId}/admin` }}
-                </button>
-              </div>
+          <div v-if="false" style="border: solid 1px red;">
+            <div>government: {{ room.government }}</div>
+            <div>adminUsers: {{ room.adminUsers }}</div>
+            <div>role: {{ role }}</div>
+            <div>users: {{ room && room.users }}</div>
+            <div v-if="currentUser">
+              <div>uid: {{ currentUser.uid }}</div>
             </div>
-
-            <p>
-              部屋ID: {{ roomId }}
-              <abutton
+            <div>
+              <button
+                @click="$router.push({ name: 'hub', params: { roomId } })"
+              >
+                RoomId: {{ roomId }}
+              </button>
+            </div>
+            <div v-if="role.manageUser">
+              <button
                 @click="$router.push({ name: 'admin', params: { roomId } })"
               >
-                設定
-                <fa-icon icon="cog"></fa-icon>
-              </abutton>
-            </p>
-            <span class="version">{{ version }}</span>
+                {{ `${roomId}/admin` }}
+              </button>
+            </div>
           </div>
 
-          <div class="share-button-container">
-            <share-button
-              :room-id="roomId"
-              :now-playing="playingMusic && playingMusic.title"
-            ></share-button>
-          </div>
+          <p>
+            部屋ID: {{ roomId }}
+            <abutton
+              @click="$router.push({ name: 'admin', params: { roomId } })"
+            >
+              設定
+              <fa-icon icon="cog"></fa-icon>
+            </abutton>
+          </p>
+          <span class="version">{{ version }}</span>
+        </div>
 
-          <div class="room-users">
-            <img v-for="u in users" :key="u.id" :src="u.photo" alt="icon" />
-          </div>
+        <div class="share-button-container">
+          <share-button
+            :room-id="roomId"
+            :now-playing="playingMusic && playingMusic.title"
+          ></share-button>
+        </div>
 
-          <template v-if="role.addViaSearch">
-            <input-area @parsed="addQueue" />
-          </template>
+        <div class="room-users">
+          <img v-for="u in users" :key="u.id" :src="u.photo" alt="icon" />
+        </div>
 
-          <div class="ad-container">
-            <ad-square></ad-square>
-          </div>
+        <div class="ad-container">
+          <ad-square></ad-square>
+        </div>
 
-          <div class="jumper">
-            <section>
-              <label>
-                <p class="label-desc">部屋の移動/作成をする</p>
-                <input
-                  type="text"
-                  v-model="jumpTo"
-                  :disabled="!currentUser"
-                  placeholder="部屋ID"
-                />
-                <abutton @click="jump" :disabled="!currentUser">移動</abutton>
-              </label>
-            </section>
-            <section>
-              <label class="checkbox">
+        <div class="jumper">
+          <label>
+            <p class="label-desc">部屋の移動/作成をする</p>
+            <input
+              type="text"
+              v-model="jumpTo"
+              :disabled="!currentUser"
+              placeholder="部屋ID"
+            />
+          </label>
+          <abutton @click="jump" :disabled="!currentUser">移動</abutton>
+          <!-- <label class="checkbox">
                 <input type="checkbox" v-model="isMonarchism" disabled="true" />
                 作成時は権限を設定できるようにする
-              </label>
-            </section>
-          </div>
-          <abutton @click="signOut" :disabled="!currentUser"
-            >サインアウト</abutton
-          >
+              </label> -->
         </div>
+        <abutton @click="signOut" v-if="!currentUser">
+          サインアウト
+        </abutton>
+        <abutton @click="$router.push('/signin')" v-else>
+          サインイン
+        </abutton>
 
-        <div class="column swiper-slide">
-          <p class="header">キュー</p>
-          <div class="no-music" v-if="queues.length === 0">
-            キューに動画がありません
-          </div>
-          <music-queue
-            v-model="queues"
-            @interrupt="interrupt"
-            :is-draggable="isDraggable"
-            :role="role"
-            class="music-list"
-            v-else
-          ></music-queue>
-        </div>
+        <template v-if="role.addViaSearch">
+          <input-area @parsed="addQueue" />
+        </template>
+      </div>
 
-        <div class="column swiper-slide">
-          <p class="header">履歴</p>
-          <template v-if="!currentUser">
-            <div class="no-music">Only available for signed in users</div>
-            <div class="button-container">
-              <abutton class="is-large" @click="$router.push('/signin')"
-                >サインイン</abutton
-              >
-            </div>
-          </template>
-          <div class="no-music" v-else-if="history.length === 0">
-            履歴に動画がありません
-          </div>
-          <history-list
-            :list="history"
-            @add="addQueue"
-            @del="deleteMusicFromHistory"
-            class="music-list"
-            :role="role"
-            v-else
-          ></history-list>
+      <div class="column swiper-slide">
+        <p class="header">キュー</p>
+        <div class="no-music" v-if="queues.length === 0">
+          キューに動画がありません
         </div>
+        <music-queue
+          v-model="queues"
+          @interrupt="interrupt"
+          :is-draggable="isDraggable"
+          :role="role"
+          class="music-list"
+          v-else
+        ></music-queue>
+      </div>
+
+      <div class="column swiper-slide">
+        <p class="header">履歴</p>
+        <template v-if="!currentUser">
+          <div class="no-music">サインインしたユーザーのみが利用できます</div>
+          <div class="button-container">
+            <abutton class="is-large" @click="$router.push('/signin')">
+              サインイン
+            </abutton>
+          </div>
+        </template>
+        <div class="no-music" v-else-if="history.length === 0">
+          履歴に動画がありません
+        </div>
+        <history-list
+          :list="history"
+          @add="addQueue"
+          @del="deleteMusicFromHistory"
+          class="music-list"
+          :role="role"
+          v-else
+        ></history-list>
       </div>
     </div>
 
