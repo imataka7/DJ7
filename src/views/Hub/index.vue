6cadbadb0822d659e8eecfee4276f690faea389e
imataka7 <imataka7@gmail.com>
2020-06-06 01:10:46 +0900

    Add .tab-item

--- src/views/Hub/index.vue
+++ src/views/Hub/index.vue
@@ -2,98 +2,102 @@
   <div class="hub">
     <div class="columns">
       <div class="column input-container">
-        <div class="room-desc">
-          <div class="dj7-logo">
-            <img :src="require('@/assets/logo.png')" alt="DJ7" />
-            <fa-icon
-              class="crown"
-              icon="crown"
-              size="lg"
-              v-if="room.isMonarchism"
-            ></fa-icon>
-          </div>
+        <div class="tab-item">
+          <div class="room-desc">
+            <div class="dj7-logo">
+              <img :src="require('@/assets/logo.png')" alt="DJ7" />
+              <fa-icon
+                class="crown"
+                icon="crown"
+                size="lg"
+                v-if="room.isMonarchism"
+              ></fa-icon>
+            </div>
 
-          <div v-if="false" style="border: solid 1px red;">
-            <div>government: {{ room.government }}</div>
-            <div>adminUsers: {{ room.adminUsers }}</div>
-            <div>role: {{ role }}</div>
-            <div>users: {{ room && room.users }}</div>
-            <div v-if="currentUser">
-              <div>uid: {{ currentUser.uid }}</div>
+            <div v-if="false" style="border: solid 1px red;">
+              <div>government: {{ room.government }}</div>
+              <div>adminUsers: {{ room.adminUsers }}</div>
+              <div>role: {{ role }}</div>
+              <div>users: {{ room && room.users }}</div>
+              <div v-if="currentUser">
+                <div>uid: {{ currentUser.uid }}</div>
+              </div>
+              <div>
+                <button
+                  @click="$router.push({ name: 'hub', params: { roomId } })"
+                >
+                  RoomId: {{ roomId }}
+                </button>
+              </div>
+              <div v-if="role.manageUser">
+                <button
+                  @click="$router.push({ name: 'admin', params: { roomId } })"
+                >
+                  {{ `${roomId}/admin` }}
+                </button>
+              </div>
             </div>
-            <div>
-              <button
-                @click="$router.push({ name: 'hub', params: { roomId } })"
-              >
-                RoomId: {{ roomId }}
-              </button>
-            </div>
-            <div v-if="role.manageUser">
-              <button
+
+            <p>
+              部屋ID: {{ roomId }}
+              <abutton
                 @click="$router.push({ name: 'admin', params: { roomId } })"
               >
-                {{ `${roomId}/admin` }}
-              </button>
-            </div>
+                設定
+                <fa-icon icon="cog"></fa-icon>
+              </abutton>
+            </p>
+            <span class="version">{{ version }}</span>
           </div>
 
-          <p>
-            部屋ID: {{ roomId }}
-            <abutton
-              @click="$router.push({ name: 'admin', params: { roomId } })"
-            >
-              設定
-              <fa-icon icon="cog"></fa-icon>
-            </abutton>
-          </p>
-          <span class="version">{{ version }}</span>
-        </div>
+          <div class="share-button-container">
+            <share-button
+              :room-id="roomId"
+              :now-playing="playingMusic && playingMusic.title"
+            ></share-button>
+          </div>
 
-        <div class="share-button-container">
-          <share-button
-            :room-id="roomId"
-            :now-playing="playingMusic && playingMusic.title"
-          ></share-button>
-        </div>
+          <div class="room-users">
+            <img v-for="u in users" :key="u.id" :src="u.photo" alt="icon" />
+          </div>
 
-        <div class="room-users">
-          <img v-for="u in users" :key="u.id" :src="u.photo" alt="icon" />
-        </div>
-
-        <div class="jumper">
-          <label>
-            <p class="label-desc">部屋の移動/作成をする</p>
-            <input
-              type="text"
-              v-model="jumpTo"
-              :disabled="!currentUser"
-              placeholder="部屋ID"
-            />
-          </label>
-          <abutton @click="jump" :disabled="!currentUser">移動</abutton>
-          <!-- <label class="checkbox">
+          <div class="jumper">
+            <label>
+              <p class="label-desc">部屋の移動/作成をする</p>
+              <input
+                type="text"
+                v-model="jumpTo"
+                :disabled="!currentUser"
+                placeholder="部屋ID"
+              />
+            </label>
+            <abutton @click="jump" :disabled="!currentUser">移動</abutton>
+            <!-- <label class="checkbox">
                 <input type="checkbox" v-model="isMonarchism" disabled="true" />
                 作成時は権限を設定できるようにする
               </label> -->
+          </div>
+
+          <abutton @click="signOut" v-if="currentUser">
+            サインアウト
+          </abutton>
+          <abutton @click="$router.push('/signin')" v-else>
+            サインイン
+          </abutton>
         </div>
 
-        <abutton @click="signOut" v-if="currentUser">
-          サインアウト
-        </abutton>
-        <abutton @click="$router.push('/signin')" v-else>
-          サインイン
-        </abutton>
+        <div class="tab-item">
+          <template v-if="role.addViaSearch">
+            <input-area @parsed="addQueue" />
+          </template>
+
+          <div class="ad-container">
+            <ad-square></ad-square>
+          </div>
+        </div>
       </div>
 
-      <template v-if="role.addViaSearch">
-        <input-area @parsed="addQueue" />
-      </template>
-
-      <div class="ad-container">
-        <ad-square></ad-square>
-      </div>
-
-      <div class="column queue-container">
+      <div class="column queue-container tab-item">
         <p class="header">キュー</p>
         <div class="no-music" v-if="queues.length === 0">
           キューに動画がありません
@@ -108,7 +112,7 @@
         ></music-queue>
       </div>
 
-      <div class="column history-container">
+      <div class="column history-container tab-item">
         <p class="header">履歴</p>
         <template v-if="!currentUser">
           <div class="no-music">サインインしたユーザーのみが利用できます</div>
@@ -134,6 +138,7 @@
 
     <player-controller
       ref="controller"
+      class="tab-item"
       @update="onStatusChanged"
       @end="onMusicEnded"
       @error="onError"
