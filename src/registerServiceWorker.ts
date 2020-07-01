177e5fd0d84b0d201cb9d031908589ca04b09260
imataka7 <imataka7@gmail.com>
2020-07-01 21:12:18 +0900

    Notify when new version is available

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -1,34 +1,49 @@
 /* eslint-disable no-console */
 
-import { register } from 'register-service-worker';
+import { register, Hooks } from 'register-service-worker';
+import Swal from 'sweetalert2';
+
+const confirmReloading = () => {
+  return Swal.fire({
+    text: '最新版があります。更新して今すぐ使おう🤟🏻',
+    toast: true,
+    confirmButtonText: '更新',
+    position: 'bottom-right',
+  });
+};
+
+const path = `${process.env.BASE_URL}service-worker.js`;
+
+const hooks: Hooks = {
+  ready() {
+    console.log(
+      'App is being served from cache by a service worker.\n'
+      + 'For more details, visit https://goo.gl/AFskqB',
+    );
+  },
+  registered() {
+    console.log('Service worker has been registered.');
+  },
+  cached() {
+    console.log('Content has been cached for offline use.');
+  },
+  updatefound() {
+    console.log('New content is downloading.');
+  },
+  updated() {
+    console.log('New content is available; please refresh.');
+    confirmReloading().then(() => window.location.reload());
+  },
+  offline() {
+    console.log(
+      'No internet connection found. App is running in offline mode.',
+    );
+  },
+  error(error) {
+    console.error('Error during service worker registration:', error);
+  },
+};
 
 if (process.env.NODE_ENV === 'production') {
-  register(`${process.env.BASE_URL}service-worker.js`, {
-    ready() {
-      console.log(
-        'App is being served from cache by a service worker.\n'
-          + 'For more details, visit https://goo.gl/AFskqB',
-      );
-    },
-    registered() {
-      console.log('Service worker has been registered.');
-    },
-    cached() {
-      console.log('Content has been cached for offline use.');
-    },
-    updatefound() {
-      console.log('New content is downloading.');
-    },
-    updated() {
-      console.log('New content is available; please refresh.');
-    },
-    offline() {
-      console.log(
-        'No internet connection found. App is running in offline mode.',
-      );
-    },
-    error(error) {
-      console.error('Error during service worker registration:', error);
-    },
-  });
+  register(path, hooks);
 }
