f66db237cbfb38534aa0741e78f99b89094fec6c
imataka7 <imataka7@gmail.com>
2020-07-13 22:23:33 +0900

    Remove a profile from a room when a user leave

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,26 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 const firestore = admin.firestore();
+const { arrayRemove } = admin.firestore.FieldValue;
+
+async function leaveRooms(uid, roomIds) {
+  const tasks = roomIds.map(id => {
+    const ref = firestore.doc(`rooms/${id}`);
+    const room = (await ref.get()).data();
+
+    const me = room.users.find(u => u.uid === uid);
+
+    if (!me) {
+      return;
+    }
+
+    return ref.update({
+      users: arrayRemove(me),
+    });
+  });
+
+  return Promise.all(tasks);
+}
 
 exports.onUserStatusChagned = functions.database.ref('/status/{uid}')
   .onUpdate(async (change, context) => {
@@ -20,5 +40,10 @@ exports.onUserStatusChagned = functions.database.ref('/status/{uid}')
 
     eventStatus.last_changed = new Date(eventStatus.last_changed);
 
-    return userStatusFirestoreRef.set(eventStatus);
+    if (eventStatus.state === 'offline') {
+      const rooms = status.rooms || [];
+      await leaveRooms(context.params.uid, rooms);
+    }
+
+    return userStatusFirestoreRef.update(eventStatus);
   });
