af9d07779c9c07286b2482befa9aba25f6d4f9f6
imataka7 <imataka7@gmail.com>
2020-08-02 23:23:50 +0900

    Disable space keybinding

--- src/components/PlayerController/script.ts
+++ src/components/PlayerController/script.ts
@@ -110,13 +110,13 @@ export default class PlayerController extends Vue {
       }
 
       // space -> toggle play and pause
-      if (code === CODE_SPACE && this.role.playerPause) {
-        if (this.currentStatus === PlayerStatus.PLAY) {
-          this.updateStatus(PlayerStatus.PAUSE);
-        } else if (this.currentStatus === PlayerStatus.PAUSE) {
-          this.updateStatus(PlayerStatus.PLAY);
-        }
-      }
+      // if (code === CODE_SPACE && this.role.playerPause) {
+      //   if (this.currentStatus === PlayerStatus.PLAY) {
+      //     this.updateStatus(PlayerStatus.PAUSE);
+      //   } else if (this.currentStatus === PlayerStatus.PAUSE) {
+      //     this.updateStatus(PlayerStatus.PLAY);
+      //   }
+      // }
 
       // left -> seek -5 sec
       if (code === CODE_LEFT && this.role.playerSeek) {
