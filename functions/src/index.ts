4c037d0e2af41c182a4e7c6feab3c278c16d5ec3
imataka7 <imataka7@gmail.com>
2020-08-16 12:55:10 +0900

    Add function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,15 +1,15 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+import functions from 'firebase-functions';
+import admin from 'firebase-admin';
 
 admin.initializeApp();
 
 const firestore = admin.firestore();
 const { arrayRemove } = admin.firestore.FieldValue;
 
-async function leaveRooms(uid, roomIds) {
+async function leaveRooms(uid: string, roomIds: string[]) {
   const tasks = roomIds.map(async id => {
     const ref = firestore.doc(`rooms/${id}`);
-    const room = (await ref.get()).data();
+    const room = (await ref.get()).data() as { users: { uid: string }[] };
 
     const me = room.users.find(u => u.uid === uid);
 
@@ -41,7 +41,7 @@ exports.onUserStatusChagned = functions.database.ref('/status/{uid}')
     eventStatus.last_changed = new Date(eventStatus.last_changed);
 
     if (eventStatus.state === 'offline') {
-      const firestoreSnapshot = (await userStatusFirestoreRef.get()).data();
+      const firestoreSnapshot = (await userStatusFirestoreRef.get()).data() as { rooms: string[] };
       const rooms = firestoreSnapshot.rooms || [];
       await leaveRooms(context.params.uid, rooms);
       eventStatus.rooms = [];
@@ -49,3 +49,15 @@ exports.onUserStatusChagned = functions.database.ref('/status/{uid}')
 
     return userStatusFirestoreRef.set(eventStatus, { merge: true });
   });
+
+exports.getVideoList = functions.https.onCall(async (data, cxt) => {
+  const uids = ['tcxUGMgaQWPyeJHdZFIJTmn0Ssz2'];
+  const list = [];
+  for (const uid of uids) {
+    const ref = await firestore.doc(`users/${uid}`).get();
+    const { history } = ref.data() as { history: any[] };
+    list.push(...history);
+  }
+
+  return list;
+});
